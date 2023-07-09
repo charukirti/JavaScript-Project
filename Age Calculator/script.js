7397444f0d80afeee577a1cc3d07de3bee5b6f01
Charukirti Chougule <108792404+charukirti@users.noreply.github.com>
2023-07-09 23:53:26 +0530

    Added age calculator project to the repository

---
+++ Age Calculator/script.js
@@ -0,0 +1,97 @@
+'use strict'
+
+const calc = document.querySelector('#calcBtn');
+
+const input = document.querySelector('#birthdate');
+
+const year = document.querySelector('#years');
+
+const month = document.querySelector('#months');
+
+const day = document.querySelector('#days');
+
+const error = document.querySelector('#error');
+
+const result = document.querySelector('.result');
+
+calc.addEventListener('click', () => {
+
+
+    /* current dates months and year */
+    const currentData = new Date();
+    console.log("This is current date ", currentData);
+    const currentDate = currentData.getDate();
+    const currentMonth = currentData.getMonth() + 1;
+    const currentYear = currentData.getFullYear();
+
+    /* Validating that date entered by the user is not euqal to the current date  */
+
+    // Getting date entered by the user
+
+    const enteredData = new Date(input.value);
+    console.log("This is entered date ", enteredData);
+
+    const enteredDate = enteredData.getDate();
+    const enteredMonth = enteredData.getMonth() + 1;
+    const enteredYear = enteredData.getFullYear();
+
+    /* Validating that date entered by the user is not euqal to the current date  */
+
+    if (
+        enteredDate === currentDate &&
+        enteredMonth === currentMonth &&
+        enteredYear === currentYear
+    ) {
+        error.style.display = 'block';
+        error.textContent = 'Date should not be equal to the current date';
+        result.style.display = 'none';
+    } else if (
+        enteredYear > currentYear ||
+        (enteredYear === currentYear && enteredMonth > currentMonth) ||
+        (enteredYear === currentYear && enteredMonth === currentMonth && enteredDate > currentDate)
+    ) {
+        error.style.display = 'block';
+        error.textContent = 'Date should not be more than current date';
+
+    }
+    else if (input.value === '') {
+        error.style.display = 'block';
+        error.textContent = 'Input field should not be empty'
+
+    }
+    else {
+        let diffYear = currentYear - enteredYear;
+
+        let diffMonth = currentMonth - enteredMonth;
+
+        let diffDate = currentDate - enteredDate;
+
+        // Adjust year and month difference if current month is less than entered month
+        if (diffMonth < 0) {
+            diffYear--;
+            diffMonth += 12;
+        }
+
+        // Adjust month and date difference if current date is less than entered date
+        if (diffDate < 0) {
+            diffMonth--;
+            var prevMonthLastDay = new Date(currentYear, currentMonth - 1, 0).getDate();
+            diffDate += prevMonthLastDay;
+        }
+        result.style.display = 'block'
+        error.textContent = '';
+        year.textContent = diffYear;
+        month.textContent = diffMonth;
+        day.textContent = diffDate;
+    }
+
+
+/* Leap year check */
+    const isLeapYear = (year) => (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+    if (isLeapYear(currentYear) && enteredMonth <= 2 && currentMonth > 2) {
+        diffDate--;
+    }
+
+
+});
+
