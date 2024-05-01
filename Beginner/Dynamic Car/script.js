40d11ec6c009b7ae509320c89cd1d809582b0e2e
charukirti <charukirticc112002@gmail.com>
2024-05-01 10:20:52 +0530

    Added all the projects in a one single file

---
+++ Beginner/Dynamic Car/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const redColor = document.querySelector(".red");
+const grayColor = document.getElementsByClassName("gray");
+const blackColor = document.querySelector(".black");
+const cartButton = document.getElementById("button");
+const itemTag = document.getElementsByTagName("h3")[0];
+const imageCard = document.querySelector(".product-image");
+const feedbackBtn = document.querySelector(".feedback");
+
+// showing car
+
+imageCard.addEventListener()
